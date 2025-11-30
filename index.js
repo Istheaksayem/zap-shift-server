1bfd14833f6d3279f0b74b94390d3747c8019167
Istheaksayem <istheakahmed9345@gmail.com>
2025-11-30 10:22:22 +0600

    added payment get api

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ async function run() {
 
     })
 
+    // payment 
+    app.get('/parcels/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query ={_id:new ObjectId(id)}
+      const result =await parcelsCollections.findOne(query)
+      res.send(result)
+
+    })
+
     app.post('/parcels',async(req,res) =>{
       const parcel =req.body;
       // parcel created time
