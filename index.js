0d5b9ffdb94c2cad81a9165a1866afc88fc55313
Istheaksayem <istheakahmed9345@gmail.com>
2025-12-02 12:33:00 +0600

    update

--- index.js
+++ index.js
@@ -126,6 +126,7 @@ async function run() {
 
       // console.log('session retrieve', session)
 
+      
 
       // api 2 bar hit na korar jorno
       const transactionId = session.payment_intent;
@@ -232,7 +233,9 @@ async function run() {
     // })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+
+    
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
